8090bae98e912437bd5a90b0fe065e2e84e28fc7
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-09-02 18:41:09 +0530

    git ignore file

---
+++ build/precache-manifest.7ca3e37c79caa86ccc24403175ea48e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65d3df18deddce53179d87daf86e194",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161862bb0774bab76f0c",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "19ddb57145daf1493d18",
+    "url": "/static/js/2.58163508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.58163508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161862bb0774bab76f0c",
+    "url": "/static/js/main.c558b883.chunk.js"
+  },
+  {
+    "revision": "55cf121f99261d6df648",
+    "url": "/static/js/runtime-main.2d9e6c14.js"
+  }
+]);
