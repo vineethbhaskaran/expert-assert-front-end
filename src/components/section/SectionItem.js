0f69775e32d36ab0dd6e5cb7e3ca22a57ba5821c
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-10-08 10:50:55 +0530

    Added state and move css class to that

--- src/components/section/SectionItem.js
+++ src/components/section/SectionItem.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 export class SectionItem extends Component {
-    handleOnClick = (currentSection) => {
-        console.log("Course:"+currentSection.course+" Section:"+currentSection._id);
-      }
+  state = {
+    currentClass: "list-group-item list-group-item-action",
+  };
+  handleOnClick = (currentSection) => {
+    console.log("Course:" + currentSection.course + " Section:" + currentSection._id);
+   
+   
+  };
 
   render() {
     return (
-      <button type="button" className="list-group-item list-group-item-action" onClick={this.handleOnClick.bind(this, this.props.section)}>
+      <button
+        type="button"
+        className={this.state.currentClass}
+        onClick={this.handleOnClick.bind(this, this.props.section)}
+      >
         {this.props.section.sectionNumber}. {this.props.section.name}
       </button>
     );
