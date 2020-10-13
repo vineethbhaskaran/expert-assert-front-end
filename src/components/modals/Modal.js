36390e0a37c5fba1d8c57462e63f5feac25daa55
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-10-13 15:09:45 +0530

    added create sectino modal

---
+++ src/components/modals/Modal.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+export class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { sectionName: "", sectionNumber: "", lessonsCount: "" };
+    this.handleClose = this.handleClose.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleClose = () => {
+    this.props.closeModal();
+  };
+
+  handleChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit() {
+    let name = this.state.sectionName;
+    let seq = this.state.sectionNumber;
+    let lessonCount = this.state.lessonsCount;
+    this.props.createSection(name, seq, lessonCount);
+  }
+
+  render() {
+    const showHideClassName = this.props.show ? "modal display-block" : "modal display-none";
+    return (
+      <div className={showHideClassName} id="createSection" tabindex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <form>
+              <div className="modal-header">
+                <h5 className="modal-title">Create New Section</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div className="form-group">
+                  <label>Section Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Section Name"
+                    name="sectionName"
+                    value={this.state.sectionName}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Section Number</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Section Number"
+                    name="sectionNumber"
+                    value={this.state.sectionNumber}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Number of Lessons</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Number of Lessons"
+                    name="lessonsCount"
+                    value={this.state.lessonsCount}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>
+                  Save Section
+                </button>
+                <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={this.handleClose}>
+                  Cancel2
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
--- src/components/section/SectionContainer.js
+++ src/components/section/SectionContainer.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchSectionsByCourse } from "../../reduxUtils/actions/sectionAction";
-import SectionItem from "./SectionItem";
 import SectionList from "./SectionList";
+import Modal from "../modals/Modal";
 
 export class SectionContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.createSection = this.createSection.bind(this);
+  }
+
+  showModal = () => {
+    this.setState({
+      show: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      show: false,
+    });
+  };
+  createSection(name, sectionNumber, numberofLesson) {
+    console.log("Section Name: " + name + " " + sectionNumber + " " + numberofLesson);
+    this.closeModal();
+  }
+
   render() {
     return (
       <div className="col-2 panel-full-height p-0 bg-grey">
-        <a href="#" className="btn btn-outline-primary .btn-block mx-1 my-1 ">
+        <button className="btn btn-outline-primary .btn-block mx-1 my-1 " onClick={this.showModal}>
           Create New Section
-        </a>
+        </button>
+        <Modal show={this.state.show} closeModal={this.closeModal} createSection={this.createSection} />
         <h6 className="px-2 pb-25 pt-25 mb-0 border-bottom font-weight-bold">SECTIONS</h6>
         <ul className="list-group list-group-flush">
           <SectionList sections={this.props.sections} />
