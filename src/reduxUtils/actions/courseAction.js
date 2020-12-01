85a79df9d87992d28d5c12d9be6354aa687bb6b0
vineeth bhaskaran <vineethbhaskaran@outlook.com>
2020-12-01 19:09:42 +0530

    change the httpResponse list to object

--- src/reduxUtils/actions/courseAction.js
+++ src/reduxUtils/actions/courseAction.js
@@ -1,51 +1,47 @@
-import { CREATE_COURSE,FETCH_COURSES,FETCH_COURSE_DETAIL } from './type';
+import { CREATE_COURSE, FETCH_COURSES, FETCH_COURSE_DETAIL } from "./type";
 import * as config from "../../config";
-import authAxios from "../../helpers/AuthHelper"
+import authAxios from "../../helpers/AuthHelper";
 
-export const fetchCourses = () => dispatch => {
-    authAxios
+export const fetchCourses = () => (dispatch) => {
+  authAxios
     .get(config.GET_ALL_COURSES_URL)
     .then((response) => {
       let httpResponse = response.data;
       dispatch({
         type: FETCH_COURSES,
-        payload: httpResponse.data
-      })
+        payload: httpResponse.data,
+      });
     })
     .catch((error) => {
       console.log(error);
     });
 };
-export const fetchCourseDetail = (courseId) => dispatch => {
-  
+export const fetchCourseDetail = (courseId) => (dispatch) => {
   authAxios
-  .get(config.GET_ALL_COURSES_URL+courseId)
-  .then((response) => {
-    let httpResponse = response.data;
-    dispatch({
-      type: FETCH_COURSE_DETAIL,
-      payload: httpResponse[0]
+    .get(config.GET_ALL_COURSES_URL + courseId)
+    .then((response) => {
+      let httpResponse = response.data;
+      dispatch({
+        type: FETCH_COURSE_DETAIL,
+        payload: httpResponse,
+      });
     })
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-export const createCourse= (courseRequest)=> dispatch =>{
+export const createCourse = (courseRequest) => (dispatch) => {
   authAxios
-    .post(config.GET_ALL_COURSES_URL,courseRequest)
+    .post(config.GET_ALL_COURSES_URL, courseRequest)
     .then((response) => {
       let httpResponse = response.data;
       dispatch({
         type: CREATE_COURSE,
-        payload: httpResponse.data
-      })
+        payload: httpResponse.data,
+      });
     })
     .catch((error) => {
       console.log(error);
     });
-  
-}
-
-
+};
